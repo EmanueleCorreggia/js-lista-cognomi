2cb9559d216b43458ab38702cd707a35d26a11eb
Emanuele <emanuele.correggia86@gmail.com>
2020-01-09 00:57:53 +0100

    end

---
+++ js/style.js
@@ -0,0 +1,16 @@
+var listaNomi = ['carlo', 'fabio', 'alessio', 'andrea', 'emanuele', 'salvatore', 'margherita', 'giovanni', 'marco', 'matteo', 'francesco'];
+var nomeUtente = prompt('Come ti chiami?');
+listaNomi.push(nomeUtente);
+var posizioneUtente;
+for (var i = 0; i < listaNomi.length; i++) {
+  listaNomi[i] = listaNomi[i].toLowerCase();
+}
+listaNomi.sort();
+posizioneUtente = listaNomi.indexOf(nomeUtente);
+for (i = 0; i < listaNomi.length; i++) {
+document.getElementById('lista').innerHTML += '<li>' + listaNomi[i] + '</li>';
+}
+document.getElementById('lista').style.listStyle = 'none';
+document.getElementById('posizione').innerHTML = 'Sei il numero ' + ++posizioneUtente + ' della lista';
+console.log(listaNomi);
+console.log('Sei il numero ' + ++posizioneUtente + ' della lista');
